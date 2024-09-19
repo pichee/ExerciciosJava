f502c36854e9d7ba13c6e7728cb7825d38c8e027
Gustavo <76635390+pichee@users.noreply.github.com>
2024-09-19 17:05:48 -0300

    Atualizar o loop

---
+++ CursoUdemy/loop/exercicio003.java
@@ -0,0 +1,22 @@
+/*
+Leia um valor inteiro.Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o 
+X, se for o caso.
+*/
+
+import java.util.Scanner;
+public class u {
+
+    public static void main(String[] args) {
+        int x=0;
+        Scanner variavel=new Scanner(System.in);
+        System.out.print("Digite um número:");
+        x=variavel.nextInt();
+        for(int i=0;i<=x;i++){
+            if(i%2==1){
+                System.out.print(i+"\n");
+           
+            }
+            
+        }
+    }
+}
