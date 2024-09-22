616fd1d95361a511ab7e655644b7b3c7348b7a21
Gustavo <76635390+pichee@users.noreply.github.com>
2024-09-22 14:51:03 -0300

    Create Uchiha.java

---
+++ CursoFiasco/exercicio003/Uchiha.java
@@ -0,0 +1,10 @@
+package exercicio;
+
+public class Uchiha extends Ninja {
+    String habilidadeEspecial="Sharingan";
+    public Uchiha() {
+    }
+    public void informacaos(){
+        System.out.println("nome:"+name+"\nidade:"+age+"\nmissao:"+missao+"\ndificuldade:"+dificuldadeMissao+"\nstatus:"+statusDamissao+"\nHabilidade:"+habilidadeEspecial);
+    }
+}
