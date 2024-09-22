4f698500fcc27ebafabb6a8d7152e9adfbe1d8b6
Gustavo <76635390+pichee@users.noreply.github.com>
2024-09-22 14:50:38 -0300

    Create Ninja.java

---
+++ CursoFiasco/exercicio003/Ninja.java
@@ -0,0 +1,11 @@
+package exercicio;
+public class Ninja {
+    String name;
+    int age;
+    String missao;
+    String dificuldadeMissao;
+    String statusDamissao;
+    public void informacao() {
+        System.out.println("nome:"+name+"\nidade:"+age+"\nmissao:"+missao+"\ndificuldade:"+dificuldadeMissao+"\nstatus:"+statusDamissao+"\n");
+    }
+}
