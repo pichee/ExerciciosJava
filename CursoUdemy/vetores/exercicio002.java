f1ca90f79dfa8cb10a2391c17b07fd2b2f5a5e0d
Gustavo <76635390+pichee@users.noreply.github.com>
2024-09-30 14:45:36 -0300

    Atualizar o vetores

---
+++ CursoUdemy/vetores/exercicio002.java
@@ -0,0 +1,29 @@
+/*
+ * Faça um programa que leia N números reais e armazene-os em um vetor. Em seguida: 
+- Imprimir todos os elementos do vetor 
+- Mostrar na tela a soma e a média dos elementos do vetor
+ */
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Digite quantos numeros voce vai digitar:");
+        int numero=sc.nextInt();
+        double soma=0;
+        double[] vetor= new double[numero];
+            for(int i=0;i<numero;i++){
+                System.out.print("Digite um número:");
+                vetor[i]=sc.nextDouble();
+                soma=soma+vetor[i];
+            }    
+        soma=soma/numero;
+        System.out.print("Todos os numeros digitados:\n");
+               
+        for(int i=0;i<numero;i++){
+                System.out.println(vetor[i]);
+                
+            } 
+         System.out.printf("A media desses numeros é %.2f%n",soma);        
+    }
+    
+}
