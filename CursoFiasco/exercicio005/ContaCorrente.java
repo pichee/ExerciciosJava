adfdbad269d0aa1ac31595cfae3d4887b5dc83ee
Gustavo <76635390+pichee@users.noreply.github.com>
2024-10-20 15:32:15 -0300

    Create ContaCorrente.java

---
+++ CursoFiasco/exercicio005/ContaCorrente.java
@@ -0,0 +1,5 @@
+public class ContaCorrente extends ContaBancaria{
+    public ContaCorrente(double dinheiroDaConta, int numeroDaconta, String nomedotitular) {
+        super(dinheiroDaConta, numeroDaconta, nomedotitular);
+    }
+}
