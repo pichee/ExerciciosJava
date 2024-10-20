c0a90d7ae742fbac27f62f17e2c03a2fb5a82f27
Gustavo <76635390+pichee@users.noreply.github.com>
2024-10-20 15:33:16 -0300

    Create Main.java

---
+++ CursoFiasco/exercicio005/Main.java
@@ -0,0 +1,8 @@
+/*Você vai criar um programa para gerenciar diferentes
+tipos de contas bancárias no Banco de Konoha. O
+objetivo é usar classes abstratas e interfaces para
+definir comportamentos comuns e específicos para
+diferentes tipos de contas.
+*/
+public class Main {
+}
